095e40b5075baf0da2459637fadefe5ecbd14aa8
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-06-29 16:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+  var boldWords = document.querySelectorAll('b');
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = 'green';
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+  var boldWords = document.querySelectorAll('b');
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = 'black';
+  }
     
 }
